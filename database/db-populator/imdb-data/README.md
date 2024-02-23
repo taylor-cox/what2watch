a4c62f0147b808ac1f18174f2d134828116216a7
taylor-cox <taylor@coxintl.com>
2024-02-23 11:13:04 -0500

    added bash script to download the imdb datasets.

--- database/db-populator/imdb-data/README.md
+++ database/db-populator/imdb-data/README.md
@@ -1,1 +1,3 @@
 You can download the IMDB datasets to populate the database with [here](https://developer.imdb.com/non-commercial-datasets/).
+
+You can also run the bash script, `get_datasets.sh`.
---
+++ database/db-populator/imdb-data/get_datasets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download datasets
+wget https://datasets.imdbws.com/name.basics.tsv.gz
+wget https://datasets.imdbws.com/title.akas.tsv.gz
+wget https://datasets.imdbws.com/title.basics.tsv.gz
+wget https://datasets.imdbws.com/title.crew.tsv.gz
+wget https://datasets.imdbws.com/title.episode.tsv.gz
+wget https://datasets.imdbws.com/title.principals.tsv.gz
+wget https://datasets.imdbws.com/title.ratings.tsv.gz
